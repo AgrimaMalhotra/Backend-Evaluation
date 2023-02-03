f743263c6dfe9e7cf4ac702075f1d38cadf6d0c6
Agrima <er.agrima.malhotra@gmail.com>
2023-02-03 16:00:55 +0530

    feat: add error handling for empty database

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -3,28 +3,34 @@ const utils = require('../utils/csvToJson');
 const services = require('../services/company');
 
 const createDb = async (req, res) => {
-  const response = await axios.get(req.body.urlLink, { responseType: 'blob' });
-  const file = response.data;
-  const jsonObj = utils.csvJson(file);
-  let companyId = [], companySector = [];
+  try {
+    const response = await axios.get(req.body.urlLink, { responseType: 'blob' });
+    const file = response.data;
+    const jsonObj = utils.csvJson(file);
+    let companyId = [], companySector = [];
 
-  for (const obj in jsonObj) {
-    companyId.push(obj.company_id);
-    companySector.push(obj.company_sector);
-  };
+    for (const obj in jsonObj) {
+      companyId.push(obj.company_id);
+      companySector.push(obj.company_sector);
+    }
 
-  for (const id of companyId) {
-    const data = await axios.get(`http://54.167.46.10/company/${id}`);
-    await services.createCompanyDetails(data.data);
+    for (const id of companyId) {
+      const companyData = await axios.get(`http://54.167.46.10/company/${id}`);
+      await services.createCompanyDetails(companyData.data);
+    }
+
+    for (const sectorName of companySector) {
+      const sectorData = await axios.get(`http://54.167.46.10/sector?name=${sectorName}`);
+      for (const obj of sectorData.data) { await services.createSectorDetails(sectorName, obj); }
+    }
+
+    const companyData = await services.getData();
+    if (companyData === null) {
+      throw new Error('No data found');
+    }
+    res.status(200).json(companyData);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-
-  for (const sectorName of companySector) {
-    const sectorData = await axios.get(`http://54.167.46.10/sector?name=${sectorName}`);
-    for (const obj of sectorData.data) { await services.createSectorDetails(sectorName, obj); }
-  }
-
-  const companyData = await services.getData();
-  res.status(200).json(companyData);
-
 }
 module.exports = { createDb, };
