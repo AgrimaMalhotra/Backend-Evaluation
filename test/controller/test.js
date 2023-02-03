4488a71e98759c927434fce18b8777064d25ffa4
Agrima <er.agrima.malhotra@gmail.com>
2023-02-03 16:00:13 +0530

    test: add no entries in database

--- test/controller/test.js
+++ test/controller/test.js
@@ -4,24 +4,24 @@ const utils = require('../../src/utils/csvToJson');
 
 jest.mock('axios');
 describe('Test Controller', () => {
+  const axios = require('axios');
+  const axiosData = '95b,IT\n95b,IT\n';
   describe('Post API', () => {
-    const axios = require('axios');
-    const axiosData = '95b,IT\n95b,IT\n';
-    const mockCompany = [{
-      id: 1,
-      companyId: '95b',
-      name: 'Volkswagen',
-      ceo: 'Marshall Kautzer'
-    }];
-    const mockSector = { 'sectorName': 'IT', 'companyId': '95b', 'score': 13.102174999999999 };
-    const mockResult = {
-      id: 1,
-      companyId: '95b',
-      name: 'Volkswagen',
-      ceo: 'Marshall Kautzer',
-      score: 13.102174999999999
-    };
     it('Should return all tasks', async () => {
+      const mockCompany = [{
+        id: 1,
+        companyId: '95b',
+        name: 'Volkswagen',
+        ceo: 'Marshall Kautzer'
+      }];
+      const mockSector = { 'sectorName': 'IT', 'companyId': '95b', 'score': 13.102174999999999 };
+      const mockResult = {
+        id: 1,
+        companyId: '95b',
+        name: 'Volkswagen',
+        ceo: 'Marshall Kautzer',
+        score: 13.102174999999999
+      };
       jest.spyOn(service, 'createCompanyDetails').mockResolvedValue(mockCompany);
       jest.spyOn(service, 'createSectorDetails').mockResolvedValue(mockSector);
       jest.spyOn(service, 'getData').mockResolvedValue(mockResult);
@@ -41,31 +41,33 @@ describe('Test Controller', () => {
       expect(mockRes.status).toBeCalledWith(200);
       expect(mockRes.json).toBeCalledWith(mockResult);
     });
-    // it('Should return 404 error', async () => {
-    //   jest.spyOn(service, 'createCompanyDetails').mockResolvedValue(null);
-    //   const mockReq = {
-    //   }
-    //   const mockRes = {
-    //     status: jest.fn().mockReturnThis(),
-    //     json: jest.fn()
-    //   }
-    //   await controller.createCompanyDetails(mockReq, mockRes);
-    //   expect(mockRes.status).toBeCalledWith(404);
-    //   expect(mockRes.json).toBeCalledWith({ message: 'No entries found' });
-    // });
-    // it('Should return 500 Error', async () => {
-    //   jest.spyOn(service, 'createCompanyDetails').mockRejectedValue(null);
-    //   const mockReq = {
-    //   }
-    //   const mockjson = jest.fn();
-    //   const mockRes = {
-    //     status: jest.fn(() => ({
-    //       json: mockjson,
-    //     }))
-    //   }
-    //   await controller.createCompanyDetails(mockReq, mockRes);
-    //   expect(mockRes.status).toBeCalledWith(500);
-    //   expect(mockjson).toBeCalledWith({ message: 'Something went wrong' });
-    // });
+    it('Should return error if empty company id', async () => {
+      const mockCompany = [{
+        id: 1,
+        companyId: null,
+        name: 'Volkswagen',
+        ceo: 'Marshall Kautzer'
+      }];
+      const mockSector = { 'sectorName': 'IT', 'companyId': null, 'score': 13.102174999999999 };
+
+      jest.spyOn(service, 'createCompanyDetails').mockResolvedValue(mockCompany);
+      jest.spyOn(service, 'createSectorDetails').mockResolvedValue(mockSector);
+      jest.spyOn(service, 'getData').mockResolvedValue(null);
+      jest.spyOn(utils, 'csvJson').mockResolvedValue([{ company_id: null, company_sector: 'IT' }, { company_id: '95b', company_sector: 'IT' }]);
+      axios.get.mockResolvedValue(axiosData);
+      const mockReq = {
+        body:
+        {
+          urlLink: 'https://store-0001.s3.amazonaws.com/input.csv'
+        }
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      await controller.createDb(mockReq, mockRes);
+      expect(mockRes.status).toBeCalledWith(400);
+      expect(mockRes.json).toBeCalledWith({ message: 'No data found' });
+    });
   });
 });
