521e08e1c30baf25c7cb2021f2cb18746d7f9bb5
Agrima <er.agrima.malhotra@gmail.com>
2023-02-06 03:08:31 +0530

    feat: add route for updation of name or ceo

--- src/routes/company.js
+++ src/routes/company.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/save', controller.createDb);
 router.get('/companies', controller.getCompanyRanking);
+router.patch('/update', controller.updateCompanyDetails);
 
 module.exports = router;
