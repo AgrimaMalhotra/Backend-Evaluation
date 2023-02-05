0078a3100de8c2b93c0fbefe23c48f3c9eafb855
Agrima <er.agrima.malhotra@gmail.com>
2023-02-05 20:55:36 +0530

    feat: add error handling for empty input

--- src/utils/csvtoJson.js
+++ src/utils/csvtoJson.js
@@ -1,13 +1,14 @@
 const csvJson = (csvStr) => {
-  var lines = csvStr.split("\n");
+  if (csvStr === '') throw new Error('csvStr is undefined');
+  var lines = csvStr.split('\n');
   var result = [];
 
-  var headers = lines[0].split(",");
+  var headers = lines[0].split(',');
 
   for (var i = 1; i < lines.length; i++) {
 
     var obj = {};
-    var currentline = lines[i].split(",");
+    var currentline = lines[i].split(',');
 
     for (var j = 0; j < headers.length; j++) {
       obj[headers[j]] = currentline[j];
