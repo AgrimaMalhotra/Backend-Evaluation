4164ff3070c0a5a21576443556c0207cee08588d
Agrima <er.agrima.malhotra@gmail.com>
2023-02-06 03:37:21 +0530

    feat: add request validation checks

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,10 +1,11 @@
 const controller = require('../controllers/company');
 const express = require('express');
+const { validationTest } = require('../middleware');
 
 const router = express.Router();
 
-router.post('/save', controller.createDb);
-router.get('/companies', controller.getCompanyRanking);
-router.patch('/update', controller.updateCompanyDetails);
+router.post('/save', validationTest('urlSchema', ''), controller.createDb);
+router.get('/companies', validationTest('querySchema', 'sector'), controller.getCompanyRanking);
+router.patch('/update', validationTest('querySchema', 'id'), validationTest('patchSchema', 'id'), controller.updateCompanyDetails);
 
 module.exports = router;
